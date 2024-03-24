3bc19a679f66289f198cf98abd3aa85fff060ba5
lauanS <lauan.s.souza@gmail.com>
2024-03-24 13:56:01 -0300

    :lipstick: Add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <RouterView />
+  <div class="main-layout">
+    <RouterView />
+  </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped>
+.main-layout {
+  background-color: #111827;
+  color: white;
+
+  min-width: 100vw;
+  min-height: 100vh;
+  padding: 20px;
+  box-sizing: border-box;
+}
 </style>
